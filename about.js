20945438f7183aaf8b7b7e17cf91cb0883021dc8
linuxlite <eze.rey92@gmail.com>
2022-05-24 20:32:59 -0300

    efecto burbujas

---
+++ about.js
@@ -0,0 +1,4 @@
+document.body.onload = () => {
+  document.body.style.backgroundImage =
+    "url('https://images.unsplash.com/photo-1565060169194-19fabf63012c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870')";
+};
---
+++ burbujas.js
@@ -0,0 +1,15 @@
+var burbujas = document.getElementById("burbujas");
+let coloresOscuros = Math.floor(Math.random() * 255);
+
+for (let index = 0; index < 1000; index++) {
+  console.log(index);
+  burbujas.innerHTML += `<div style="left:${
+    1 + Math.floor(Math.random() * 90)
+  }%;
+  animation: burbujas ${1 + Math.floor(Math.random() * 1000)}s linear infinite ;
+  background-color: rgb(${coloresOscuros}, ${coloresOscuros}, ${coloresOscuros},0.7);
+
+  
+
+  " class="burbuja"></div>`;
+}
