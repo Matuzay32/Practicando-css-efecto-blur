9c43bebb2335331fe6f010fc097c65284340e747
linuxlite <eze.rey92@gmail.com>
2022-05-26 20:48:14 -0300

    cambios con imagenes y seccion de about ' git push

--- burbujas.js
+++ burbujas.js
@@ -6,7 +6,7 @@ for (let index = 0; index < 1000; index++) {
   burbujas.innerHTML += `<div style="left:${
     1 + Math.floor(Math.random() * 90)
   }%;
-  animation: burbujas ${1 + Math.floor(Math.random() * 1000)}s linear infinite ;
+  animation: burbujas ${1 + Math.floor(Math.random() * 60)}s linear infinite ;
   background-color: rgb(${coloresOscuros}, ${coloresOscuros}, ${coloresOscuros},0.7);
 
   
--- regiter.js
+++ regiter.js
@@ -1,4 +1,4 @@
 document.body.onload = () => {
   document.body.style.backgroundImage =
-    "url('https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873')";
+    "url('https://images.unsplash.com/photo-1653226439336-177c4c1ccc51?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870')";
 };
