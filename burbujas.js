fefe4a80e18bbc3f91eab2e1e6e73b1d2f516c94
linuxlite <eze.rey92@gmail.com>
2022-05-26 21:39:03 -0300

    nueva anim y mejorando esl script

--- burbujas.js
+++ burbujas.js
@@ -1,15 +1,26 @@
 var burbujas = document.getElementById("burbujas");
-let coloresOscuros = Math.floor(Math.random() * 255);
 
-for (let index = 0; index < 1000; index++) {
-  console.log(index);
-  burbujas.innerHTML += `<div style="left:${
-    1 + Math.floor(Math.random() * 90)
-  }%;
-  animation: burbujas ${1 + Math.floor(Math.random() * 60)}s linear infinite ;
-  background-color: rgb(${coloresOscuros}, ${coloresOscuros}, ${coloresOscuros},0.7);
+const creacionBurbuja = (
+  burbujas,
+  velocidad = 60,
+  posicion = 90,
+  coloresGrises = 255,
+  tamaño = 5,
+  cantidad = 1
+) => {
+  for (let index = 0; index < cantidad; index++) {
+    const posicionBurbuja = Math.floor(Math.random() * posicion);
+    const coloresOscuros = Math.floor(Math.random() * coloresGrises);
+    const sizeBurbuja = Math.floor(Math.random() * tamaño);
+    const velocidadBurbujas = 1 + Math.floor(Math.random() * velocidad);
 
-  
+    burbujas.innerHTML += `<div style="left:${posicionBurbuja}%;
+    animation: burbujas ${velocidadBurbujas}s linear infinite ;
+    background-color: rgb(${coloresOscuros}, ${coloresOscuros}, ${coloresOscuros},0.7);
+    width:${sizeBurbuja}px;
+    height:${sizeBurbuja}px;
+    " class="burbuja"></div>`;
+  }
+};
 
-  " class="burbuja"></div>`;
-}
+creacionBurbuja(burbujas, 10, 100, 300, 30, 100);
